12cedaf429685fb1a4597a1e7e74ed9ba9a826d2
Daniel Kukula <daniel.kuku@gmail.com>
2023-05-05 22:04:33 +0100

    remove inspect overrides

--- lib/inspect_overrides.ex
+++
@@ -1,4 +0,0 @@
-ignore? = Code.compiler_options()[:ignore_module_conflict]
-Code.compiler_options(ignore_module_conflict: true)
-Code.require_file("inspect_overrides/overrides.ex")
-Code.compiler_options(ignore_module_conflict: ignore?)
